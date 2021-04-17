35022112d37bcc4642bc1b9a447fa3461237f38d
Hugo Lundin <hugo@lundin.dev>
2021-04-17 15:04:04 +0200

    Minor changes

--- sonosctl/app/sonos/views.py
+++ sonosctl/app/sonos/views.py
@@ -1,11 +1,18 @@
 from . import sonos
 from . import system
+from time import sleep
 from flask import render_template, Response, abort, url_for
+
+WAIT_DURATION = 0.3
 
 @sonos.route('/group')
 def group():
     try:
         system.group()
+        system.set_volume()
+        system.set_status_light(True)
+        sleep(WAIT_DURATION)
+        system.set_status_light(False) 
     except Exception as e:
         print(e)
         abort(500)
@@ -16,6 +23,9 @@ def group():
 def ungroup():
     try:
         system.ungroup()
+        system.set_status_light(True)
+        sleep(WAIT_DURATION)
+        system.set_status_light(False)
     except Exception as e:
         print(e)
         abort(500)
@@ -26,7 +36,7 @@ def ungroup():
 def speakers():
     return system.get_speakers()
 
-@sonos.route('/speakers/volume/<int:percentage>')
+@sonos.route('/volume/<int:percentage>')
 def volume(percentage):
     if percentage < 0 or percentage > 100:
         abort(500)
@@ -34,12 +44,12 @@ def volume(percentage):
     system.set_volume(percentage)
     return Response(status=200)
 
-@sonos.route('/speakers/status_light/on')
+@sonos.route('/status_light/on')
 def status_light_on():
     system.set_status_light(light_on=True)
     return Response(status=200)
 
-@sonos.route('/speakers/status_light/off')
+@sonos.route('/status_light/off')
 def status_light_off():
     system.set_status_light(light_on=False)
     return Response(status=200)
